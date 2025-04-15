0f51f117f8f58c5799a30dc4a019f2a357f76013
Edwin Hdz <edwinhotmail663@gmail.com>
2025-04-15 02:29:41 -0600

    Código ordenado

--- src/main/java/ConsultaMonedaAPI.java
+++ src/main/java/ConsultaMonedaAPI.java
@@ -1,3 +1,26 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsultaMonedaAPI {
-    //public
+    public  Moneda convertirMoneda(String divisa1,String divisa2,Double monto){
+        String cadena = "https://v6.exchangerate-api.com/v6/da743706fa5920ee9d34bd30/pair/" +
+                divisa1+"/"+divisa2+"/"+monto;
+        HttpClient cliente = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(cadena))
+                .build();
+
+        try{
+            HttpResponse<String> response = cliente
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return new Gson().fromJson(response.body(),Moneda.class);
+        }catch (Exception e){
+            throw new RuntimeException("No se pudo convertir las monedas");
+        }
+    }
 }
--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -8,6 +8,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.text.NumberFormat;
 import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -15,13 +16,8 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner consola = new Scanner(System.in);
-        HashMap<Integer,String> divisas = new HashMap<>();
-        divisas.put(1,"ARS");
-        divisas.put(2,"BOB");
-        divisas.put(3,"BRL");
-        divisas.put(4,"CLP");
-        divisas.put(5,"COP");
-        divisas.put(6,"USD");
+        HashMap<Integer,String> divisas = inicializarDivisas();
+        ConsultaMonedaAPI consulta = new ConsultaMonedaAPI();
 
         int opcion=0;
         String divisa1=null,divisa2=null;
@@ -31,77 +27,77 @@ public class Principal {
 
         while(true)
         {
-            System.out.println("""
-                    \tSeleccione el numero de la divisa para convertir
-                    1- (ARS) Peso argentino
-                    2- (BOB) Boliviano boliviano
-                    3- (BRL) Real brasileño
-                    4- (CLP) Peso chileno
-                    5- (COP) Peso colombiano 
-                    6- (USD) Dólar estadounidence 
-                    7- Salir
-                    """);
-            opcion = Integer.parseInt(consola.nextLine());
-            switch (opcion)
+            opcion =0;
+            try{
+                menu(opcion);
+                opcion = Integer.parseInt(consola.nextLine());
+                switch (opcion)
+                {
+                    case 1 -> divisa1 = divisas.get(opcion);
+                    case 2 -> divisa1 = divisas.get(opcion);
+                    case 3 -> divisa1 = divisas.get(opcion);
+                    case 4 -> divisa1 = divisas.get(opcion);
+                    case 5 -> divisa1 = divisas.get(opcion);
+                    case 6 -> divisa1 = divisas.get(opcion);
+                    case 7 -> divisa1 = divisas.get(opcion);
+                    case 8 -> System.exit(0);
+                    default -> System.out.println("Opción inválida. Por favor, ingrese un número entre 1 y 8");
+                }
+
+                menu(opcion);
+                opcion = Integer.parseInt(consola.nextLine());
+                switch (opcion)
+                {
+                    case 1 -> divisa2 = divisas.get(opcion);
+                    case 2 -> divisa2 = divisas.get(opcion);
+                    case 3 -> divisa2 = divisas.get(opcion);
+                    case 4 -> divisa2 = divisas.get(opcion);
+                    case 5 -> divisa2 = divisas.get(opcion);
+                    case 6 -> divisa2 = divisas.get(opcion);
+                    case 7 -> divisa2 = divisas.get(opcion);
+                    case 8 -> System.exit(0);
+                    default -> System.out.println("Opción inválida. Por favor, ingrese un número entre 1 y 8");
+                }
+
+                System.out.println("Ingresa la cantidad en "+divisa1+" para convertir en " + divisa2);
+                monto = Double.parseDouble(consola.nextLine());
+
+                Moneda conversion = consulta.convertirMoneda(divisa1,divisa2,monto);
+                System.out.println("\n"+monto +" "+ conversion.base_code() +
+                        " equivale a " + conversion.conversion_result() +" " +conversion.target_code());
+
+            }catch (NumberFormatException e)
             {
-                case 1 -> divisa1 = divisas.get(opcion);
-                case 2 -> divisa1 = divisas.get(opcion);
-                case 3 -> divisa1 = divisas.get(opcion);
-                case 4 -> divisa1 = divisas.get(opcion);
-                case 5 -> divisa1 = divisas.get(opcion);
-                case 6 -> divisa1 = divisas.get(opcion);
-                case 7 -> System.exit(0);
+                System.out.println("Opción inválida. Por favor, ingrese un número entre 1 y 8");
             }
-            System.out.println("""
-                    \tSeleccione el numero de la divisa a convertir
-                    1- (ARS) Peso argentino
-                    2- (BOB) Boliviano boliviano
-                    3- (BRL) Real brasileño
-                    4- (CLP) Peso chileno
-                    5- (COP) Peso colombiano 
-                    6- (USD) Dólar estadounidence 
-                    7- Salir
-                    """);
-            opcion = Integer.parseInt(consola.nextLine());
-            switch (opcion)
-            {
-                case 1 -> divisa2 = divisas.get(opcion);
-                case 2 -> divisa2 = divisas.get(opcion);
-                case 3 -> divisa2 = divisas.get(opcion);
-                case 4 -> divisa2 = divisas.get(opcion);
-                case 5 -> divisa2 = divisas.get(opcion);
-                case 6 -> divisa2 = divisas.get(opcion);
-                case 7 -> System.exit(0);
-            }
-            System.out.println("Ingresa la cantidad en "+divisa1+" para convertir en " + divisa2);
-            monto = Double.parseDouble(consola.nextLine());
-
-            String url = "https://v6.exchangerate-api.com/v6/da743706fa5920ee9d34bd30/pair/"+
-                    divisa1+"/"+divisa2+"/"+monto;
-            HttpClient cliente = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .build();
-            HttpResponse<String> response = cliente
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-
-            String json = response.body();
-            //System.out.println(json);
-
-            Gson gson =new GsonBuilder()
-                    .create();
-
-            Moneda conversion = gson.fromJson(json,Moneda.class);
-            System.out.println(monto +" "+ conversion.base_code() +
-                    " equivale a " + conversion.conversion_result() +" " +conversion.target_code());
-
-            System.out.println(conversion);
+            
+            System.out.println("\n\nPresione cualquier tecla para continuar...");
+            consola.nextLine();
         }
-
-
     }
-    private static void menu()
-    {
-
+    private static void menu(int opcion) {
+        System.out.println("""
+                    \tSeleccione el número de la divisa """+(opcion==0?"para convertir\n":"a convertir\n") +
+                    (opcion != 1?"1- (ARS) Peso argentino\n":"")+
+                    (opcion != 2?"2- (BOB) Boliviano boliviano\n":"")+
+                    (opcion != 3?"3- (BRL) Real brasileño\n":"")+
+                    (opcion != 4?"4- (CLP) Peso chileno\n":"")+
+                    (opcion != 5?"5- (COP) Peso colombiano\n":"")+
+                    (opcion != 6?"6- (USD) Dólar estadounidence\n":"")+
+                    (opcion != 7?"7- (MXN) Peso méxicano\n":"")+
+                    """
+                    8- Salir
+                    """);
+    }
+    private static HashMap<Integer,String> inicializarDivisas() {
+        HashMap<Integer,String> divisas = new HashMap<>();
+        divisas.put(1,"ARS");
+        divisas.put(2,"BOB");
+        divisas.put(3,"BRL");
+        divisas.put(4,"CLP");
+        divisas.put(5,"COP");
+        divisas.put(6,"USD");
+        divisas.put(7,"MXN");
+        return divisas;
     }
 }
